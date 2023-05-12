6c90d36d0bdcb9613dcf99b61cebe3dfe882f79e
AmarNagim <99069493@mydavinci.nl>
2023-05-13 01:54:36 +0200

    first commit
    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace PokemonBattleSimulator {
+
+    class Program {
+
+            static void Main(string[] args){
+
+                Console.WriteLine("Enter name for your Charmander: ");
+                string name = Console.ReadLine();
+
+                Charmander Charmander = new Charmander(name, "fire", "weakness");
+
+            }
+
+    }
+    
+    class Charmander {
+        public String name;
+        public String strength;
+        public String weakness;
+
+        public Charmander(String name, String strength, String weakness)
+        {   
+            this.name = name;
+            this.strength = strength;
+            this.weakness = weakness;
+        }
+    }
+
+}
