10a3395effc31da0060199dca54af0c10f91c942
AmarNagim <99069493@mydavinci.nl>
2023-05-15 15:33:10 +0200

    Assignment 1
    update 1

--- obj/Release/net7.0/Pokemon Battle Simulator c#.AssemblyInfo.cs
+++ obj/Release/net7.0/Pokemon Battle Simulator c#.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemon Battle Simulator c#")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemon Battle Simulator c#")]
